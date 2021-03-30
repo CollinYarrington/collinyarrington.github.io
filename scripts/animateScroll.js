fc4051321aa181eb8fa3e3e301fbe334f29d217a
root <root@DESKTOP-I1G5P4J.localdomain>
2021-03-30 23:44:08 +0200

    update

---
+++ scripts/animateScroll.js
@@ -0,0 +1,29 @@
+
+window.addEventListener('load', function () { 
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    // threshold: 1,
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries,
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+
+});
