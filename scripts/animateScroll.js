fc63d41d8f708a02d9459b680c6f37090e13f7e5
root <root@DESKTOP-I1G5P4J.localdomain>
2021-04-05 13:54:51 +0200

    update

--- scripts/animateScroll.js
+++ scripts/animateScroll.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () { 
 const faders = document.querySelectorAll('.fade-in');
 const profile = document.querySelectorAll('#pic');
-// const profile_image = document.getElementById('pic');
+
 
 const appearOptions = {
     // threshold: 1,
@@ -28,79 +28,18 @@ const appearOnScroll = new IntersectionObserver
 },
 appearOptions);
 
-// const profileOptions = {
-//     threshold: 1,
-// };
-// const profileMove = new IntersectionObserver
-// (function(
-//     entries,
-//     profileMove
-// ) {
-//     entries.forEach(entry => {
-//         if(!entry.isIntersecting){
-//             return;
-//         } else {
-//             entry.target.classList.add('test');
-//             profileMove.unobserve(entry.target);
-//         }
-//     });
-// },
-// profileOptions);
-
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
-
-// profile.forEach(pro => {
-//     appearOnScroll.observe(pro);
-// });
-
 });
 
-// window.addEventListener('scroll', function () {
-//     var elem = document.querySelector('#pic');
-//     var bounding = elem.getBoundingClientRect();
-//     var pic_move = false;
-
-//     if (bounding["top"] < -150.5){
-//         pic_move = true;
-//     }else if (bounding["top"] > -150.5){
-//         pic_move = false;
-//     }
-
-//     console.log(bounding["y"]);
-//     if(bounding["y"] > window.innerHeight){
-//         // document.getElementById("pic").classList.remove("return_pic");
-        
-//         document.getElementById("pic").classList.add("move_pic");
-//     }else{
-//         // document.getElementById("pic").
-//         // document.getElementById("pic").classList.remove("move_pic");
-//     }
-
-
-    
-// });
-
-
-
-// window.onscroll = function(e) {
-//     if (window.pageYOffset == 700){
-//         console.log("hit");
-//     }
-//  }
-
-  
 var profile_state = true;
 window.addEventListener('scroll', () => {
     var myElement = document.getElementById('pic');
     var bounding = myElement.getBoundingClientRect();
-    console.log(document.documentElement.scrollTop);
-    // var profile_state = false;
+    
     if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= window.innerWidth && bounding.bottom <= window.innerHeight && profile_state == true) {
-        // console.log('Element is in the viewport!');
-        // profile_state = true;
-        // document.getElementById("pic").classList.remove("move_pic");  
+       null;
     } else {
         console.log('Element is NOT in the viewport!');
         profile_state = false;
